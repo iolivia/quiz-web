fd889bee2dc5e872d91c4996dfcb4c8b750d25d9
Olivia <olivia_github@outlook.com>
2018-11-02 15:34:39 +0000

    Selecting options

--- quiz-web/src/QuestionOption.tsx
+++ quiz-web/src/QuestionOption.tsx
@@ -6,21 +6,52 @@ export interface QuestionOptionProps {
 }
 
 // tslint:disable-next-line:no-empty-interface
-export interface QuestionOptionState {}
+export interface QuestionOptionState {
+    isHovered: boolean;
+    isSelected: boolean;
+}
 
 export default class QuestionOption extends React.Component<QuestionOptionProps, QuestionOptionState> {
+
+    constructor(props: QuestionOptionProps) {
+        super(props);
+        this.state = {
+            isHovered: false,
+            isSelected: false,
+        };
+    }
 
     public render() {
 
         const { text } = this.props;
+        const { isSelected, isHovered } = this.state;
+
+        const icon = isSelected || isHovered  ? "check-circle" : "circle";
 
         return (
-            <div className="question-option-container">
-                 <FontAwesomeIcon icon={["far", "circle"]} />
-                 <FontAwesomeIcon icon={["far", "check-circle"]} />
-                * {text}
+            <div 
+                className="question-option-container" 
+                onClick={this.onClick} 
+                onMouseEnter={this.onMouseEnter}
+                onMouseLeave={this.onMouseLeave}
+                >
+                <FontAwesomeIcon icon={["far", icon]} />
+                <div className="question-option-text">
+                    {text}
+                </div>
             </div>
         );
     }
 
+    private onClick = () => {
+        this.setState({ isSelected: !this.state.isSelected, isHovered: false });
+    }
+
+    private onMouseEnter = () => {
+        this.setState({ isHovered: true });
+    }
+
+    private onMouseLeave = () => {
+        this.setState({ isHovered: false });
+    }
 }
